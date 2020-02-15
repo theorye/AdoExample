cb91daf32f8e5b32e0875a6e99354e4c57705066
Adrian <limitx3m@outlook.com>
2020-02-14 20:53:19 -0600

    all there is

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ADOCRUD.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ADOCRUD.Controllers
+{
+    public class HomeController : Controller
+    {
+        string ConnectionInformation = "Server=DESKTOP-P55BTG2\\SQLEXPRESS;Database=ADOCRUD;Trusted_Connection=True;MultipleActiveResultSets=True";
+        SqlDataReader myReader;
+
+        public IActionResult Index()
+        {
+            UserInfo UI = new UserInfo();
+            SqlConnection MainConnection = new SqlConnection(ConnectionInformation);
+
+            MainConnection.Open();
+            // string MyCommand = "SELECT * FROM UserInfo";
+            // SqlCommand myCommand = new SqlCommand(MyCommand, MainConnection);
+            SqlCommand myCommand = new SqlCommand("UserInfoProcedure", MainConnection);
+            myCommand.Parameters.Add(new SqlParameter("@Filter", "Read"));
+            myCommand.CommandType = CommandType.StoredProcedure;
+            myReader = myCommand.ExecuteReader();
+
+            while(myReader.Read())
+            {
+                UI.UserName = myReader.GetValue(1).ToString();
+            }
+            MainConnection.Close();
+
+            
+            return View(UI);
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserInfo UI)
+        {
+            /* == ADO COMMAND == */
+            SqlConnection MainConnection = new SqlConnection(ConnectionInformation);
+
+            MainConnection.Open();
+            string MyCommand = "Insert Into UserInfo (UserName) values ('" + UI.UserName + "')";
+            SqlCommand myCommand = new SqlCommand(MyCommand, MainConnection);
+            myCommand.ExecuteNonQuery();
+            MainConnection.Close();
+            return View(UI);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateUserInfo(UserInfo UI)
+        {
+            /* == ADO COMMAND == */
+            SqlConnection MainConnection = new SqlConnection(ConnectionInformation);
+
+            MainConnection.Open();
+            string MyCommand = "Update UserInfo set UserName='" + UI.NewUserName + "' where UserName ='" + UI.UserName + "' ";
+            SqlCommand myCommand = new SqlCommand(MyCommand, MainConnection);
+            myCommand.ExecuteNonQuery();
+            MainConnection.Close();
+            return View(UI);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteUserInfo(UserInfo UI)
+        {
+            /* == ADO COMMAND == */
+            SqlConnection MainConnection = new SqlConnection(ConnectionInformation);
+
+            MainConnection.Open();
+            string MyCommand = "Delete from UserInfo where UserName='" + UI.UserName + "'";
+            SqlCommand myCommand = new SqlCommand(MyCommand, MainConnection);
+            myCommand.ExecuteNonQuery();
+            MainConnection.Close();
+            return View(UI);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SQL/StoredProcedure.sql
@@ -0,0 +1,54 @@
+﻿-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE UserInfoProcedure
+	-- Add the parameters for the stored procedure here
+	@Filter varchar(50),
+	@UserName varchar(50) = null,
+	@NewUserName varchar(50) = null
+
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	If @Filter = 'Create'
+	BEGIN
+		Insert into UserInfo (UserName) values (@UserName);
+	END
+	ELSE
+	If @Filter = 'Read'
+	BEGIN
+		SELECT * FROM UserInfo;
+	END
+	ELSE
+	IF @Filter = 'Update'
+	BEGIN
+		UPDATE UserInfo SET UserName = @NewUserName WHERE UserName = @UserName;
+	END
+	ELSE
+	IF @Filter = 'Delete'
+	BEGIN
+		DELETE FROM UserInfo WHERE UserName = @UserName;
+	END
+END
+GO
